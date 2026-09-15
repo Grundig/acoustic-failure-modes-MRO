aa29545394060095d14bf88eb9e2f85cb52d79a4
kamil-grunwald <kam.grunwald@gmail.com>
2026-09-15 12:02:06 +0200

    [processing] class

--- afm/processing/processing.py
+++ afm/processing/processing.py
@@ -2,18 +2,23 @@ from tkinter import filedialog
 from afm.importers.reading_data import AudioLoader
 from afm.visualisation.plots import Plots
 
+class Processor:
+    def __init__(self):
+        pass
 
-def load():
-    loader = AudioLoader()
-    audio, sample_rate = loader.load_audio(filedialog.askopenfilename())
+    @staticmethod
+    def load():
+        loader = AudioLoader()
+        audio, sample_rate = loader.load_audio(filedialog.askopenfilename())
 
-    return audio, sample_rate
+        return audio, sample_rate
 
 
-def main():
-    audio, sample_rate = load()
-    Plots.plot_spectrogram(audio, sample_rate, vmax=0.01)
+    def main(self):
+        audio, sample_rate = self.load()
+        Plots.plot_spectrogram(audio, sample_rate, vmax=0.01)
 
 
 if __name__ == "__main__":
-    main()
+    p = Processor()
+    p.main()
