e2791919735731d9292838906f609866751dd257
kamil-grunwald <kam.grunwald@gmail.com>
2026-09-21 09:33:18 +0200

    [loading] read batch data

--- afm/main/main.py
+++ afm/main/main.py
@@ -3,7 +3,6 @@ from matplotlib import pyplot as plt
 from afm.importers.reading_data import AudioLoader
 from afm.processing.processing import Processor
 from afm.visualisation.plots import Plots
-from afm.machine_learning.embedding import Embedding
 from tkinter import filedialog
 
 import torch
@@ -15,22 +14,25 @@ class Main:
         self.loader = AudioLoader()
         self.processor = Processor()
         self.plots = Plots()
-        self.embedding = Embedding()
 
-        self.filepath = filedialog.askopenfilename()
+        self.filepath = filedialog.askdirectory()
         self.waveform = None
         self.sample_rate = None
+        self.load()
 
     def load(self):
-        self.waveform, self.sample_rate = self.loader.load_audio(self.filepath)
+        audio_list = self.loader.load_audio(self.filepath, num_frames=-1)
+        if audio_list:
+            self.waveform, self.sample_rate = audio_list[0]
+        else:
+            raise ValueError(f"No audio files found in: {self.filepath}")
 
-    def run(self):
-        self.load()
+    def visualise(self):
         log_mel = self.processor.get_log_mel_spectrum(self.waveform, self.sample_rate)
         spectr = self.processor.get_spectrum(self.waveform)
         pitch = self.processor.get_pitch(self.waveform, self.sample_rate)
 
-        fig, ax = plt.subplots(3,1)
+        fig, ax = plt.subplots(3, 1)
         self.plots.plot_waveform(self.waveform, self.sample_rate, ax=ax[0])
         self.plots.plot_spectrogram(log_mel, ax=ax[1], title="Log-mel Spectrogram")
         self.plots.plot_spectrogram(spectr, ax=ax[2], title="Spectrogram")
@@ -38,8 +40,10 @@ class Main:
         fig.tight_layout()
         plt.show()
 
-        extracted_features = self.embedding.process(self.waveform)
-        print(extracted_features)
+    def run(self):
+        pass
+
+
 
 if __name__ == "__main__":
     main = Main()
