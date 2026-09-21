71188f012fb9b238751fee55fb82b46b228b65f7
kamil-grunwald <kam.grunwald@gmail.com>
2026-09-21 09:33:09 +0200

    [loading] read batch data

--- afm/importers/reading_data.py
+++ afm/importers/reading_data.py
@@ -6,34 +6,48 @@ class AudioLoader:
     def __init__(self):
         pass
 
-    def load_audio(self, file_path: str | Path, mono: bool = True):
+    @staticmethod
+    def load_audio(path: str | Path, mono: bool = True, num_frames: int = -1, file_extension: str = ".wav") -> list[tuple[torch.Tensor, int]]:
         """
         Args:
-            file_path: Path to the audio file.
+            path: Path to the audio file or directory.
             mono: If True, convert audio to mono.
+            num_frames: Number of frames to load.
+            file_extension: If path is a directory, load files with this extension.
 
         Returns:
-            A tuple containing:
-                - audio_samples: NumPy array of normalized float32 samples in range [-1.0, 1.0]
+            A list of tuples, each containing:
+                - audio_samples: Tensor of samples
                 - sample_rate: Sampling rate of the audio file
         """
-        file_path = Path(file_path)
+        path = Path(path)
 
-        if not file_path.exists():
-            raise FileNotFoundError(f"Audio file not found: {file_path}")
+        if not path.exists():
+            raise FileNotFoundError(f"Audio file or directory not found: {path}")
 
-        waveform, sample_rate = torchaudio.load(file_path, num_frames=10000)
-        if mono and waveform.shape[0] > 1:
-            waveform = torch.mean(waveform, dim=0, keepdim=True)
+        audio_list = []
+        if path.is_file():
+            files = [path]
+        elif path.is_dir():
+            files = sorted(list(path.glob(f"*{file_extension}")))
+        else:
+            raise ValueError(f"Path is neither a file nor a directory: {path}")
 
-        return waveform, sample_rate
+        for file_path in files:
+            waveform, sample_rate = torchaudio.load(file_path, num_frames=num_frames)
+            if mono and waveform.shape[0] > 1:
+                waveform = torch.mean(waveform, dim=0, keepdim=True)
+            audio_list.append((waveform, sample_rate))
+
+        return audio_list
 
 
 if __name__ == "__main__":
     from tkinter import filedialog
     loader = AudioLoader()
-    audio_data, fs = loader.load_audio(filedialog.askopenfilename())
+    audio_list = loader.load_audio(filedialog.askopenfilename())
 
-    print(f"Sample rate: {fs}")
-    print(f"Audio shape: {audio_data.shape}")
-    print(f"Audio dtype: {audio_data.dtype}")
+    for audio_data, fs in audio_list:
+        print(f"Sample rate: {fs}")
+        print(f"Audio shape: {audio_data.shape}")
+        print(f"Audio dtype: {audio_data.dtype}")
